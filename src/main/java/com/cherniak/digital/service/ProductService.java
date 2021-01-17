e941f9891c5fadf3d8cb983dd4c01911fec71f03
Viktor Cherniak <barsetca@gmail.com>
2021-01-17 11:47:47 +0300

    added get All method with Cache (identityMap) and fix saveOrUpdate

--- src/main/java/com/cherniak/digital/service/ProductService.java
+++ src/main/java/com/cherniak/digital/service/ProductService.java
@@ -2,30 +2,32 @@ package com.cherniak.digital.service;
 
 import com.cherniak.digital.model.Product;
 import com.cherniak.digital.repository.ProductRepository;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
-
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
-  public static Map<Long,Product> identityMap = new HashMap<>();
+  public static Map<Long, Product> identityMap = new HashMap<>();
 
   private final ProductRepository repository;
 
   @Transactional
   public Product saveOrUpdate(Product product) {
-   identityMap.replace(product.getId(), product);
-    return repository.save(product);
+    Product saved = repository.save(product);
+    identityMap.put(saved.getId(), saved);
+    return saved;
   }
 
-  public Product get(Long id){
-    if (identityMap.containsKey(id)){
+  public Product get(Long id) {
+    if (identityMap.containsKey(id)) {
       return identityMap.get(id);
     }
     Product product = repository.findById(id).orElse(null);
@@ -36,8 +38,18 @@ public class ProductService {
   }
 
   @Transactional
-  public void delete(Long id){
+  public void delete(Long id) {
     identityMap.remove(id);
     repository.deleteById(id);
+  }
+
+  public List<Product> getAll() {
+    int count = repository.getCount();
+    if (count != identityMap.size()) {
+      repository.findAll().stream()
+          .filter(p -> !identityMap.containsKey(p.getId()))
+          .forEach(p -> identityMap.put(p.getId(), p));
+    }
+    return new ArrayList<>(identityMap.values());
   }
 }
